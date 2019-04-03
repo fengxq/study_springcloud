c8802a237361b5bc92ee79c97fe5ccb8f9168a4f
fengxinqiang <fxq230@163.com>
2019-04-03 17:14:23 +0800

    增加Ribbon Feign

--- README.md
+++ README.md
@@ -29,8 +29,9 @@ mvn install:install-file -Dfile=D:\ojdbc6\11.2.0\ojdbc6-11.2.0.jar -DgroupId=com
 
 SpringCloud和SpringBoot针对JSP页面的目录结构是不一样的。Cloud：main\resources\META-INF\resources\WEB-INF。Boot：main\webapp\WEB-INF
 
-fallback与fallbackFactory区别，fallback只是出错运行你指定的方法，拿不到错误信息，fallbackFactory可以拿到错误信息
-
+fallback与fallbackFactory区别，fallback只是出错运行你指定的方法，拿不到错误信息，fallbackFactory可以拿到错误信息。
+调用远程需要借助单独的类和接口，feign是编写一个接口，而ribbon是编写一个类，在这个类中我们就可以通过resttemplate来调
+用远程服务。
 Eureka
 Feign
 Ribbon
--- pay/src/main/java/fz/fxq/pay/service/UserFeignService.java
+++ pay/src/main/java/fz/fxq/pay/service/UserFeignService.java
@@ -5,6 +5,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+/**
+ * fallback与fallbackFactory区别，fallback只是出错运行你指定的方法，拿不到错误信息，fallbackFactory可以拿到错误信息。
+ */
 //@FeignClient(value = "user-service",fallback = UserFeignServiceImpl.class)
 @FeignClient(value = "user-service", fallbackFactory = UserFeignFactoryServiceImpl.class)
 public interface UserFeignService {
