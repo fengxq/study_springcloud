044ab948ba8e843ca7f4620c96b604dd5b38701f
fengxinqiang <fxq230@163.com>
2019-04-04 17:01:17 +0800

    增加Config Client

--- pay/src/main/java/fz/fxq/pay/PayApplication.java
+++ pay/src/main/java/fz/fxq/pay/PayApplication.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
@@ -15,6 +14,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableHystrix
 @EnableHystrixDashboard
 @EnableFeignClients
+//@EnableCircuitBreaker
 public class PayApplication {
     static Logger logger = LoggerFactory.getLogger(PayApplication.class);
 
