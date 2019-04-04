95d39ba42a107e44f90987f6d09f8dcaa260212f
fengxinqiang <fxq230@163.com>
2019-04-04 17:25:08 +0800

    增加Ribbon Feign

--- pay/src/main/java/fz/fxq/pay/service/UserFeignService.java
+++ pay/src/main/java/fz/fxq/pay/service/UserFeignService.java
@@ -11,6 +11,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 //@FeignClient(value = "user-service",fallback = UserFeignServiceImpl.class)
 @FeignClient(value = "user-service", fallbackFactory = UserFeignFactoryServiceImpl.class)
 public interface UserFeignService {
-    @GetMapping("userService/user1/info/get/{userId}")
+    @GetMapping("userService/user/info/get/{userId}")
     public String getUserInfo(@PathVariable String userId);
 }
