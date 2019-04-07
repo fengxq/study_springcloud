e62be5cefb95377f9b9b490cc3fc9cd46798b8d1
fengxinqiang <fxq230@163.com>
2019-04-07 18:02:21 +0800

    Config增加自动更新RabbitMQ

--- pay/src/main/java/fz/fxq/pay/controller/UserController.java
+++ pay/src/main/java/fz/fxq/pay/controller/UserController.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("user")
+@RefreshScope
 public class UserController {
     Logger logger = LoggerFactory.getLogger(this.getClass());
 
