1f3380b8ada3fd2e8f28a3f57d9d80659de4934f
fengxinqiang <fxq230@163.com>
2019-04-12 18:03:17 +0800

    增加商品starter

---
+++ commodity-starter/src/main/java/fz/fxq/commoditystarter/CommodityStarterApplication.java
@@ -0,0 +1,13 @@
+package fz.fxq.commoditystarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommodityStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommodityStarterApplication.class, args);
+    }
+
+}
