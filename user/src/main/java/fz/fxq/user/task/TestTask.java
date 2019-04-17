bd59b4f44e6c51a082d814312601f7436d74b02b
fengxinqiang <fxq230@163.com>
2019-04-17 16:41:21 +0800

    增加定时任务

---
+++ user/src/main/java/fz/fxq/user/task/TestTask.java
@@ -0,0 +1,22 @@
+package fz.fxq.user.task;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+/**
+ * cron表达式详解
+ *
+ */
+@Configuration
+@EnableScheduling
+public class TestTask {
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Scheduled(cron = "0 0/1 * * * ?")
+    public void test(){
+        logger.info("TestTask.................................");
+    }
+}
