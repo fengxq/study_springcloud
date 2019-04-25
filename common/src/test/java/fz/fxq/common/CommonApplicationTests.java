f766cd3a705baa165c94bf2bfa3f1da71d4f8647
fengxinqiang <fxq230@163.com>
2019-04-25 16:16:17 +0800

    common模块，文件对比

--- common/src/test/java/fz/fxq/common/CommonApplicationTests.java
+++ common/src/test/java/fz/fxq/common/CommonApplicationTests.java
@@ -9,7 +9,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.StringUtils;
 
 import java.io.*;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -20,22 +19,36 @@ public class CommonApplicationTests {
     static Logger logger = LoggerFactory.getLogger(CommonApplication.class);
 
     @Test
-    public void match() throws IOException {
-        File whiteFile = new File("F:\\whiteResult.txt");
-        File blackFile = new File("F:\\blackResult.txt");
+    public void test() throws IOException {
+        int type = 1;
 
-        File inFile = blackFile;
-        File outFile = new File("F:\\blackResult100000.txt");
+        File inFile = null;
+        File outFile = null;
 
+        if (type == 1) {
+            inFile = new File("F:\\whiteResult.txt");
+            outFile = new File("F:\\whiteResult1200000.txt");
+        } else if (type == 2) {
+            inFile = new File("F:\\blackResult.txt");
+            outFile = new File("F:\\blackResult1200000.txt");
+        }
         long startMillis = System.currentTimeMillis();
         FileInputStream fileInputStream = new FileInputStream(inFile);
         InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
         BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
         String line = "";
         FileWriter fileWriter = new FileWriter(outFile);
+        long count = 0;
         while ((line = bufferedReader.readLine()) != null) {
+            count++;
 //            logger.info(line);
-            fileWriter.write(line);
+            if (count < 1200000) {
+                continue;
+            }
+            fileWriter.write("count" + count + line + "\r\n");
+            if (count > 1300000) {
+                break;
+            }
         }
         fileWriter.close();
         bufferedReader.close();
@@ -43,7 +56,7 @@ public class CommonApplicationTests {
         fileInputStream.close();
         long endMillis = System.currentTimeMillis();
 
-        logger.info((endMillis - startMillis) + "毫秒   " );
+        logger.info((endMillis - startMillis) + "毫秒   ");
 
     }
 
@@ -62,16 +75,16 @@ public class CommonApplicationTests {
 
     @Test
     public void test2() throws Exception {
-        File whiteFile = new File("F:\\whiteResult.txt");
-        File blackFile = new File("F:\\blackResult.txt");
+        File whiteFile = new File("F:\\whiteResult.txt");//D:\$RECYCLE.BIN
+        File blackFile = new File("F:\\blackResult.txt");//F:\$RECYCLE.BIN
 
         long startMillis = System.currentTimeMillis();
 
         long whiteMapSize = 0;
         long blackMapSize = 0;
-        for (int i = 0; i < 50; i++) {
-            Map<String, String> whiteMap = read(whiteFile, i * 100000, (i + 1) * 100000 + 10000);
-            Map<String, String> blackMap = read(blackFile, i * 100000, (i + 1) * 100000 + 10000);
+        for (int i = 0; i < 1; i++) {
+            Map<String, String> whiteMap = read(whiteFile, i * 100000, (i + 1) * 6000000);
+            Map<String, String> blackMap = read(blackFile, i * 100000, (i + 1) * 6000000);
 
         /*Map<String, String> whiteMap = new ConcurrentHashMap<>();
         whiteMap.put("a", "1");
@@ -96,7 +109,7 @@ public class CommonApplicationTests {
             long endMillis = System.currentTimeMillis();
 
             whiteMapSize = whiteMapSize + whiteMap.size();
-            blackMapSize = blackMapSize + whiteMap.size();
+            blackMapSize = blackMapSize + blackMap.size();
             logger.info((endMillis - startMillis) + "whiteMap   " + whiteMapSize);
             logger.info((endMillis - startMillis) + "blackMap   " + blackMapSize);
 
@@ -105,17 +118,22 @@ public class CommonApplicationTests {
 
             FileWriter fileWriterWhite = new FileWriter(whiteFile1);
             for (String key : whiteMap.keySet()) {
-                fileWriterWhite.write(key + "\r\n");
+                fileWriterWhite.write("D:\\" + key + "\r\n");
             }
             fileWriterWhite.close();
 
             File blackFile1 = new File("F:\\blackResult" + i + ".txt");
             FileWriter fileWriterBlack = new FileWriter(blackFile1);
             for (String key : blackMap.keySet()) {
-                fileWriterBlack.write(key + "\r\n");
+                fileWriterBlack.write("F:\\" + key + "\r\n");
             }
             fileWriterBlack.close();
         }
+    }
+
+    @Test
+    public void test3(){
+
     }
 
     public Map<String, String> read(File file, long beginCount, long endCount) throws Exception {
@@ -132,7 +150,13 @@ public class CommonApplicationTests {
             if (count <= beginCount) {
                 continue;
             }
-            map.put(line, "1");
+
+            line = line.substring(1);
+            map.put(line, count + "");
+
+            if (count % 10000 == 0) {
+                logger.error("count===================" + count);
+            }
             if (count >= endCount) {
                 break;
             }
